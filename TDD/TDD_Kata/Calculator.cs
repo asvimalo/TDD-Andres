98f3ee28e2569008e4a78779ad6db952b99ce07b
asvimalo <asvimalo@gmail.com>
2017-05-27 14:53:25 +0200

    TDD Kata Calculator class

---
+++ TDD/TDD_Kata/Calculator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDD_Kata
+{
+    public class Calculator
+    {
+        public int Add(string numbers)
+        {
+            //string[] split = null;
+            char[] delim = { ',','\n',';'};
+
+            if (String.IsNullOrEmpty(numbers))
+            {
+                return 0;
+            }
+            else if (numbers.Length == 1)
+            {
+                var number = 0;
+                Int32.TryParse(numbers, out number);
+                return number;
+            }
+            else
+            {
+                return numbers.Split(delim).Sum(x => int.Parse(x));
+            }
+                
+                 
+        }
+    }
+}
