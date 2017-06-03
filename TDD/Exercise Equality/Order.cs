d89a7b0da369c326ae245520ba98404031b3266e
asvimalo <asvimalo@gmail.com>
2017-06-03 10:23:24 +0200

    Add a new exercise Equality

---
+++ TDD/Exercise Equality/Order.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_Equality
+{
+
+    public class Order : IComparable<Order>, IEquatable<Order> 
+    {
+        public int OrderId;
+        public List<OrderLine> orderLines { get; private set; }
+
+        public Order()
+        {
+            orderLines = new List<OrderLine>();
+        }
+
+        public void AddOrderLine(OrderLine orderline)
+        {
+            if (!orderLines.Contains(orderline))
+                orderLines.Add(orderline);
+        }
+        //public int CompareTo(Order other)
+        //{
+        //    
+            
+            
+        //}
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj as Order);
+        }
+        public bool Equals(Order other)
+        {
+            int response = 0;
+            bool answer = false;
+            if (other == null)
+                return false;
+            else if (this.OrderId != other.OrderId)
+                return false;
+            else if (this.OrderId == other.OrderId)
+            {
+                foreach (var order2 in other.orderLines)
+                {
+                    foreach (var order1 in orderLines)
+                    {
+                        response = order1.Name.CompareTo(order2.Name);
+                        if (response == 0)
+                        {
+                            response = order1.Price.CompareTo(order2.Price);
+                            if (response == 0)
+                            {
+                                answer = true;
+                            }
+                            else
+                                answer = false;
+
+                        }
+                        else
+                            answer = false;
+                    }
+
+                }
+                return answer;
+            }
+            //return response;
+            //&&
+            //    this.orderLines.Select(x => x.Name) == 
+            //    other.orderLines.Select(x => x.Name))                
+
+            else
+                return false;
+        }
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return ((OrderId != 0 ? OrderId.GetHashCode() : 0) * 397); //^
+                //(orderLines != null ? orderLines.GetHashCode() : 0);
+            }
+        }
+
+        public int CompareTo(Order other)
+        {
+            
+            int response = 0;
+            if (OrderId != other.OrderId)
+            {
+                response = OrderId.CompareTo(other.OrderId);
+            }
+            if (OrderId == other.OrderId)
+            {
+                foreach (var order2 in other.orderLines)
+                {
+                    foreach (var order1 in orderLines)
+                    {
+                        response = order1.Name.CompareTo(order2.Name);
+                        if (response == 0)
+                        {
+                            response = order1.Price.CompareTo(order2.Price);
+                            
+                        }
+                        //return response;
+                    }
+                    
+                }
+            }
+            return response;
+
+
+        }
+    }
+
+    public class OrderLine : IComparable<OrderLine> , IEquatable<OrderLine>
+    {
+
+        public string Name;
+        public decimal Price;
+        public int Quantity;
+
+
+        public int CompareTo(OrderLine other)
+        {
+            int result = Name.CompareTo(other.Name);
+            if (result == 0)
+            {
+                result = Price.CompareTo(other.Price);
+            }
+            return result;
+        }
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj as OrderLine);
+        }
+        public bool Equals(OrderLine other)
+        {
+            if (other == null)
+                return false;
+            if (this.Name == other.Name && 
+                this.Price == other.Price && 
+                this.Quantity == other.Quantity)
+                return true;
+            else
+                return false;
+        }
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return ((Name != null ? Name.GetHashCode() : 0) * 397) ^
+                (Price != 0 ? Price.GetHashCode() : 0);
+            }
+        }
+    }
+}
---
+++ TDD/Exercise Equality/UnitTests.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Exercise_Equality
+{
+    [TestFixture]
+    public class UnitTests
+    {
+        private Order order1;
+        private Order order2;
+        private Order order3;
+
+        [SetUp]
+        public void Setup()
+        {
+            order1 = new Order() { OrderId = 1 };
+            order1.AddOrderLine(new OrderLine() { Name = "Laptop HP", Price = 123m, Quantity = 101 });
+            order1.AddOrderLine(new OrderLine() { Name = "Speaker", Price = 12m, Quantity = 101 });
+
+            order2 = new Order() { OrderId = 1 }; ; //Order 2 is the same as order 1
+            order2.AddOrderLine(new OrderLine() { Name = "Laptop HP", Price = 123m, Quantity = 101 });
+            order2.AddOrderLine(new OrderLine() { Name = "Speaker", Price = 12m, Quantity = 101 });
+
+            order3 = new Order() { OrderId = 2 }; //Different order
+            order3.AddOrderLine(new OrderLine() { Name = "Apple Iphone", Price = 103m, Quantity = 101 });
+            order3.AddOrderLine(new OrderLine() { Name = "Lamp", Price = 20m, Quantity = 100 });
+        }
+
+        [Test]
+        public void TwoOrdersWithTheSameContentShouldEqual()
+        {
+            var equals = order1.Equals(order2);
+
+            Assert.AreEqual(order1, order2);
+        }
+
+        [Test]
+        public void TwoOrdersWithDifferentContentShouldNotEqual()
+        {
+            Assert.AreNotEqual(order1, order3);
+            Assert.AreNotEqual(order2, order3);
+        }
+
+        [Test]
+        public void TwoOrdersWithTheSameContentShouldCountAsOneInAHashSet()
+        {
+            var set = new HashSet<Order>();
+            set.Add(order1);
+            set.Add(order2);
+            set.Add(order3);
+
+            Assert.IsTrue(set.Count == 2);
+        }
+
+        [Test]
+        public void RemovingOrdersShouldWork()
+        {
+            var orderList = new List<Order>();
+            orderList.Add(order1);
+            orderList.Add(order3);
+
+            orderList.Remove(order2);   //Remove Order #2, as it is same as #1, then it should remove order 1
+
+            Assert.IsTrue(orderList.Count == 1);
+        }
+
+
+        [Test]
+        public void TwoIdenticalOrdersShouldHaveTheSameHashCode()
+        {
+            Assert.IsTrue(order1.GetHashCode() == order2.GetHashCode());
+        }
+
+        [Test]
+        public void DupliateOrderLinesIsNotAllowed()
+        {
+            var order4 = new Order() { OrderId = 4 };
+            order4.AddOrderLine(new OrderLine() { Name = "Laptop HP", Price = 123m, Quantity = 101 });
+            order4.AddOrderLine(new OrderLine() { Name = "Laptop HP", Price = 123m, Quantity = 101 });
+            order4.AddOrderLine(new OrderLine() { Name = "Laptop HP", Price = 123m, Quantity = 101 });
+
+            Assert.IsTrue(order4.orderLines.Count == 1, "Error: Order4 should contain 1 item, contains " + order4.orderLines.Count);
+        }
+
+
+        [Test]
+        public void AListOfOrdersShouldBesortable()
+        {
+            var orderList = new List<Order>();
+            orderList.Add(order1);
+            orderList.Add(order3);
+
+            orderList.Sort();  //Should not crash!
+        }
+
+        [Test]
+        public void AListOfOrdersShouldBesortableByOrderId()
+        {
+            var orderList = new List<Order>();
+            orderList.Add(order3);
+            orderList.Add(order1);
+
+            orderList.Sort();  //We sort by OrderID!
+
+            //Should be in the correct order
+            Assert.IsTrue(orderList[0].OrderId==1);
+            Assert.IsTrue(orderList[1].OrderId==2);
+        }
+
+
+        //---------------------------------------------------
+        //Stretch tasks, Solve these if you have time
+
+        [Test]
+        public void UsingEqualsShouldWork()
+        {
+            var test = order1.CompareTo(order2);
+            Assert.IsTrue(order1 == order2);
+        }
+
+        [Test]
+        public void UsingNotEqualsShouldWork()
+        {
+            Assert.IsTrue(order1 != order3);
+        }
+
+        [Test]
+        public void UsingNotEqualsShouldReturnFalseForDuplicateOrders()
+        {
+            Assert.IsFalse(order1 != order2);
+        }
+
+    }
+}
